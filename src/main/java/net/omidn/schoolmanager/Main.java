0c895fb351074d2710c458e65bdff9eb88682685
Omid.N <nejadabbasio@gmail.com>
2021-04-17 01:53:22 +0430

    added a simple fxml file to begin with

--- src/main/java/net/omidn/schoolmanager/Main.java
+++ src/main/java/net/omidn/schoolmanager/Main.java
@@ -1,9 +1,15 @@
 package net.omidn.schoolmanager;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -12,8 +18,9 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Scene scene = new Scene(new Pane(), 600, 400);
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("stage-login.fxml"));
+        Scene scene = new Scene(root, 600, 400);
 
         primaryStage.setScene(scene);
         primaryStage.show();
