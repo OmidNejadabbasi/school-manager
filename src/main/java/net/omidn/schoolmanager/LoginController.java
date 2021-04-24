00c1f83c36dddf5fd549e6084f02eeae04996dfd
Omid.N <nejadabbasio@gmail.com>
2021-04-24 16:21:01 +0430

    added login button

---
+++ src/main/java/net/omidn/schoolmanager/LoginController.java
@@ -0,0 +1,19 @@
+package net.omidn.schoolmanager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class LoginController {
+
+    @FXML
+    TextField userNameField;
+
+    @FXML
+    TextField passwordField;
+
+    @FXML
+    Button loginBtn;
+
+}
